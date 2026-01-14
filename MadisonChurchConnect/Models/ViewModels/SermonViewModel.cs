6f602e82fee14026d1b12e0b99542388d723b8bb
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-01-14 13:11:07 -0700

    restructured project

---
+++ MadisonChurchConnect/Models/ViewModels/SermonViewModel.cs
@@ -0,0 +1,13 @@
+﻿/* 
+ * Molly Gilchrist
+ * STG-446
+ * 1/14/2026
+ * Capstone Project
+ */
+
+namespace MadisonChurchConnect.Models.ViewModels
+{
+    public class SermonViewModel
+    {
+    }
+}
