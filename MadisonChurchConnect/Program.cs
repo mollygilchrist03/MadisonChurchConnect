102e66b696b4886940c21debbb9aa0ce2d7e158c
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-01-21 14:04:45 -0700

    edits

--- MadisonChurchConnect/Program.cs
+++ MadisonChurchConnect/Program.cs
@@ -1,7 +1,15 @@
+using MadisonChurchConnect.Services.BusinessLogic;
+using MadisonChurchConnect.Services.DataAccess;
+using MadisonChurchConnect.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
+
+// Register dependency injection services
+builder.Services.AddScoped<INoteDAO, NoteDAO>();
+builder.Services.AddScoped<NoteLogic>();
 
 var app = builder.Build();
 
