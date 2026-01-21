b3eb9b740bc77981be7a9d9d083dea5390fdb878
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-01-21 12:22:05 -0700

    created interface for note operations

--- MadisonChurchConnect/Services/Interfaces/INoteDAO.cs
+++ MadisonChurchConnect/Services/Interfaces/INoteDAO.cs
@@ -5,9 +5,44 @@
  * Capstone Project
  */
 
+using MadisonChurchConnect.Models.DomainModels;
+
 namespace MadisonChurchConnect.Services.Interfaces
 {
-    public class INoteDAO
+    public interface INoteDAO
     {
+        /// <summary>
+        /// adds a new note to the database
+        /// </summary>
+        /// <param name="newNote"></param>
+        /// <returns></returns>
+        int AddNewNote(NoteDomainModel newNote);
+
+        /// <summary>
+        /// retrieves a specific note by its id
+        /// </summary>
+        /// <param name="noteId"></param>
+        /// <returns></returns>
+        NoteDomainModel GetNoteById(int noteId);
+
+        /// <summary>
+        /// gets all saved notes from the database
+        /// </summary>
+        /// <returns></returns>
+        List<NoteDomainModel> GetAllNotes();
+
+        /// <summary>
+        /// deletes a note by id
+        /// </summary>
+        /// <param name="noteId"></param>
+        /// <returns></returns>
+        int DeleteNote(int noteId);
+
+        /// <summary>
+        /// allows users to edit saved notes
+        /// </summary>
+        /// <param name="noteId"></param>
+        /// <returns></returns>
+        int EditNote(int noteId);
     }
 }
