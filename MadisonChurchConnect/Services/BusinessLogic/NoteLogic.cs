cd211941a76224373a50d9c702e5fe0213e039f3
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-01-21 13:12:15 -0700

    added methods to note logic

--- MadisonChurchConnect/Services/BusinessLogic/NoteLogic.cs
+++ MadisonChurchConnect/Services/BusinessLogic/NoteLogic.cs
@@ -5,9 +5,73 @@
  * Capstone Project
  */
 
+using MadisonChurchConnect.Models.DomainModels;
+using MadisonChurchConnect.Models.ViewModels;
+using MadisonChurchConnect.Services.Interfaces;
+
 namespace MadisonChurchConnect.Services.BusinessLogic
 {
     public class NoteLogic
     {
+        // class level variables
+        private INoteDAO _noteDAO;
+
+        /// <summary>
+        /// parameterized constructor
+        /// </summary>
+        /// <param name="noteDAO"></param>
+        public NoteLogic(INoteDAO noteDAO)
+        {
+            _noteDAO = noteDAO;
+        }
+
+        /// <summary>
+        /// adds a new note from a note view model
+        /// </summary>
+        /// <param name="newNote"></param>
+        /// <returns></returns>
+        public int AddNewNote(NoteViewModel newViewNote)
+        {
+            
+        }
+
+        /// <summary>
+        /// retrieves a specific note by its id
+        /// </summary>
+        /// <param name="noteId"></param>
+        /// <returns></returns>
+        public NoteViewModel GetNoteById(int noteId)
+        {
+
+        }
+
+        /// <summary>
+        /// gets all saved notes from the database
+        /// </summary>
+        /// <returns></returns>
+        public List<NoteViewModel> GetAllNotes()
+        {
+
+        }
+
+        /// <summary>
+        /// deletes a note by id
+        /// </summary>
+        /// <param name="noteId"></param>
+        /// <returns></returns>
+        public int DeleteNote(int noteId)
+        {
+
+        }
+
+        /// <summary>
+        /// allows users to edit saved notes
+        /// </summary>
+        /// <param name="noteId"></param>
+        /// <returns></returns>
+        public int EditNote(NoteViewModel updatedViewNote)
+        {
+
+        }
     }
 }
