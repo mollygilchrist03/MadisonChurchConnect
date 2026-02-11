18c0a35c32c0896e3a492587d18ce48c7e98eb12
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-02-10 19:40:06 -0700

    added citation and comment

--- MadisonChurchConnect/Controllers/SermonsController.cs
+++ MadisonChurchConnect/Controllers/SermonsController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*
+ * Molly Gilchrist
+ * 2/9/2026
+ * STG-456
+ * Capstone Project
+ */
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace MadisonChurchConnect.Controllers
 {
     public class SermonsController : Controller
     {
+        /// <summary>
+        /// returns the default view for the action.
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
