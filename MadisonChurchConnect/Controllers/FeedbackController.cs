4e2407fe9d6dc1ca26c15c67be64c3a84eb23087
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-03-17 14:30:51 -0700

    added feeedback form

---
+++ MadisonChurchConnect/Controllers/FeedbackController.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Molly Gilchrist
+ * 3/17/2026
+ * STG-456
+ * Capstone Project
+ */
+using Microsoft.AspNetCore.Mvc;
+namespace MadisonChurchConnect.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
