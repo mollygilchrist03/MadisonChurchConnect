111f7322895f4557e47a743b2472144bda23edeb
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-03-17 14:00:07 -0700

    created menu functions

---
+++ MadisonChurchConnect/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Molly Gilchrist
+ * 3/17/2026
+ * STG-456
+ * Capstone Project
+ */
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadisonChurchConnect.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
