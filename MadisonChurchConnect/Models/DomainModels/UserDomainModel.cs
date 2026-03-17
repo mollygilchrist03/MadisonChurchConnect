707395ed6d9bc255938e257a42e8293ebd9c1f35
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-03-17 12:12:12 -0700

    added user models

---
+++ MadisonChurchConnect/Models/DomainModels/UserDomainModel.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Molly Gilchrist
+ * 3/17/2026
+ * STG-456
+ * Capstone Project
+ */
+
+namespace MadisonChurchConnect.Models.DomainModels
+{
+    public class UserDomainModel
+    {
+        // class level properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// default constructor for user domain model
+        /// </summary>
+        public UserDomainModel()
+        {
+            Id = 0;
+            FirstName = "";
+            LastName = "";
+            Username = "";
+            PasswordHash = "";
+            Email = "";
+            PhoneNumber = null;
+        }
+    }
+}
---
+++ MadisonChurchConnect/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Molly Gilchrist
+ * 3/17/2026
+ * STG-456
+ * Capstone Project
+ */
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MadisonChurchConnect.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        // class level properties
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string PasswordHash { get; set; }
+
+        [Phone(ErrorMessage = "Please enter a valid phone number.")]
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// default constructor for user view model
+        /// </summary>
+        public UserViewModel()
+        {
+            Id = 0;
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            Username = "";
+            PasswordHash = "";
+            PhoneNumber = null;
+        }
+    }
+}
