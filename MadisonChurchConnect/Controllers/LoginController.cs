a0f99944261878625f6cfdf6f360384b8c2beed7
Molly Gilchrist <mgilchrist@my.gcu.edu>
2026-03-18 13:47:27 -0700

    methods converted to async

--- MadisonChurchConnect/Controllers/LoginController.cs
+++ MadisonChurchConnect/Controllers/LoginController.cs
@@ -40,7 +40,7 @@ namespace MadisonChurchConnect.Controllers
         /// post method to process the login form
         /// </summary>
         [HttpPost]
-        public IActionResult Login(UserViewModel user)
+        public async Task<IActionResult> Login(UserViewModel user)
         {
             // declare variables
             bool isValidated = false;
@@ -64,12 +64,12 @@ namespace MadisonChurchConnect.Controllers
                 claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
 
                 // sign the user in and store the claims principal in a cookie
-                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal).Wait();
-
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+                
                 // redirect to the sermons page after login
                 return RedirectToAction("Index", "Sermons");
             }
-
+            
             // add an error message and reload the login page
             ModelState.AddModelError("", "Your username or password was incorrect. Please try again.");
             return View(user);
@@ -79,11 +79,11 @@ namespace MadisonChurchConnect.Controllers
         /// post method to log the user out
         /// </summary>
         [HttpPost]
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
             // sign the user out and clear the cookie
-            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme).Wait();
-
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            
             // redirect to the menu page
             return RedirectToAction("Index", "Menu");
         }
